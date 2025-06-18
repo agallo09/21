ab98fc6b32338ce2aad9c4e35127d4c8a36c04b1
Mathias Alejandro Gallo Bedon <alejandrogallo@Mathiass-MacBook-Air.local>
2025-06-18 07:52:24 -0500

    first commit from base

--- Game.py
+++ Game.py
@@ -7,3 +7,18 @@ import random
 # TIME TO PLAY, the house will get a turn before the players
 #the players decide to hit or stand
 # Ask the player name and age to be sure they can play
+if __name__ == '__main__':
+    print('Welcome to 21 Blackjack')
+    num_players = int(input('Enter the number of players:'))
+    players = {}
+    for i in range(num_players):
+        player_name = input(f'Enter your name player {i+1}:')
+        player_age = int(input(f'Enter your age player {i+1}:'))
+        if player_age <= 20:
+            print('This player does not have the required age to play, sorry!')
+        else:
+            players[player_name] = player_age
+    print(players)
+
+
+    
