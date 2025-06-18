45e1fc4672b3891909dd022bd67ad06fd6ea1b88
Alejandro Gallo <148939144+agallo09@users.noreply.github.com>
2025-06-18 07:24:01 -0500

    Update Game.py

--- Game.py
+++ Game.py
@@ -1,1 +1,9 @@
 import random
+#21 game flow
+#Player have to decide to hit or stand, input 
+#the house will follow certain rules to ask for more cards
+#start game
+#shuffle deck
+# TIME TO PLAY, the house will get a turn before the players
+#the players decide to hit or stand
+# Ask the player name and age to be sure they can play
